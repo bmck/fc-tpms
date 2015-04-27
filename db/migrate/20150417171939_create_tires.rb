fb2d4f88693ff1f804037d4673a7a3f68b7de15f
Bill McKinnon <bill.mckinnon@bmck.org>
2015-04-27 04:13:15 +0000

    added option for using vs owning companies

--- db/migrate/20150417171939_create_tires.rb
+++ db/migrate/20150417171939_create_tires.rb
@@ -6,7 +6,8 @@ class CreateTires < ActiveRecord::Migration
     create_table :tires do |t|
       t.integer     :sensor_id, null: false
       t.integer     :tire_type_id, null: false
-      t.integer     :company_id, null: false
+      t.integer     :using_company_id, null: false
+      t.integer     :owning_company_id, null: false
       t.string      :serial
 
       t.timestamps null: false
