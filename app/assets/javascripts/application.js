0b6909155bf82feefd545e3c99cf4283169c5350
Bill McKinnon <bill.mckinnon@bmck.org>
2015-05-05 14:46:08 +0000

    added amcharts javascript files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,17 @@
 //= require bootstrap-sprockets
 //= require bootstrap-hover-dropdown
 //= require smart_listing
+
+//= require amcharts/amcharts
+//= require amcharts/funnel
+//= require amcharts/gauge
+//= require amcharts/gantt
+//= require amcharts/pie
+//= require amcharts/radar
+//= require amcharts/serial
+//= require amcharts/xy
+//= require amcharts/plugins/dataloader/dataloader
+//= require amcharts/plugins/export/export
+//= require amcharts/plugins/responsive/responsive
+
 //= require_tree .
