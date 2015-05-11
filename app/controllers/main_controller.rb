802de23ebc6d8d9483c663d8ac70b9d6b62e49a1
Bill McKinnon <bill.mckinnon@bmck.org>
2015-05-11 15:59:14 +0000

    prevent repeating You are logged in

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,8 @@
 class MainController < ApplicationController
   def index
     if user_signed_in?
-      flash.notice = "You are logged in, #{current_user.first_last}. #{flash.notice}"
+      flash.notice = "You are logged in, #{current_user.first_last}. #{flash.notice}" unless \
+        "You are logged in".in?(flash.notice)
     else
       render action: :home
     end
