ce564fc8593848f34311f4d56163ca23eef0cb6a
Bill McKinnon <bill.mckinnon@bmck.org>
2015-08-15 21:17:33 +0000

    chgs for production server

--- server/log_macros.h
+++ server/log_macros.h
@@ -4,10 +4,10 @@
 #ifndef __LOG_MACROS_H
 #define __LOG_MACROS_H
 
-#include "file_log.h"
+// #include "file_log.h"
 
 // #if NDK_DEBUG == 0
-// #include "no_log.h"
+#include "no_log.h"
 // #endif
 
 #endif
