c9dcab83a1b5cbd70c0b465047178c28428bbf7b
Bill McKinnon <bill.mckinnon@bmck.org>
2015-09-07 20:21:20 +0000

    added fc_version

--- server/vsftpver.h
+++ server/vsftpver.h
@@ -1,7 +1,12 @@
+// $Id$
+// $(c)$
+
 #ifndef VSF_VERSION_H
 #define VSF_VERSION_H
 
-#define VSF_VERSION "0.2.0"
+#include "../fc_version.h"
+
+#define VSF_VERSION FC_VERSION
 #define VSF_SERVER_NAME "FleetServer"
 
 #endif /* VSF_VERSION_H */
