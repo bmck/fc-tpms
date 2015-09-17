244ef0ec8e2630bb56c9a757bf3f7bfc550a0a62
Bill McKinnon <bill.mckinnon@bmck.org>
2015-09-17 18:55:35 +0000

    added settings for ARCA

--- android/remote_tpms/app/src/main/java/com/fleetcents/remote_tpms/fleetcentstpmsremoteclient/SettingsFragment.java
+++ android/remote_tpms/app/src/main/java/com/fleetcents/remote_tpms/fleetcentstpmsremoteclient/SettingsFragment.java
@@ -17,8 +17,8 @@ public class SettingsFragment extends PreferenceFragment {
             addPreferencesFromResource(R.xml.pref_basestation);
             addPreferencesFromResource(R.xml.pref_units);
             addPreferencesFromResource(R.xml.pref_detailed_msgs);
+            addPreferencesFromResource(R.xml.pref_crash_report);
             addPreferencesFromResource(R.xml.pref_about);
-
         }
 
 }
