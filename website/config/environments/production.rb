8990c758f5682cf55b80b1682c0432b747cad0d0
Bill McKinnon <bill.mckinnon@bmck.org>
2015-09-20 20:49:18 +0000

    tweaks per http://stackoverflow.com/questions/24995228/fonts-in-assets-pipeline-is-not-referred-with-fingerprint-in-the-final-css

--- website/config/environments/production.rb
+++ website/config/environments/production.rb
@@ -3,6 +3,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.assets.enable = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
--- website/config/initializers/assets.rb
+++ website/config/initializers/assets.rb
@@ -9,4 +9,21 @@ Rails.application.config.assets.version = '1.00001'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile << %w(*.png *.jpg *.jpeg *.gif *.svg *.ico *.eot *.ttf *.wfoff)
+# Rails.application.config.assets.precompile << %w(*.png *.jpg *.jpeg *.gif *.svg *.ico *.eot *.ttf *.wfoff)
+
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js|eot|svg|ttf|woff)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    full_path.starts_with? app_assets_path
+    # if full_path.starts_with? app_assets_path
+    #   puts "including asset: " + full_path
+    #   true
+    # else
+    #   puts "excluding asset: " + full_path
+    #   false
+    # end
+  else
+    false
+  end
+end
