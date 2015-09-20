99668cd781a188e4b7a07b08f2f4d3dde3004864
Bill McKinnon <bill.mckinnon@bmck.org>
2015-09-20 21:19:45 +0000

    tweaks

--- website/config/initializers/assets.rb
+++ website/config/initializers/assets.rb
@@ -12,7 +12,7 @@ Rails.application.config.assets.version = '1.00001'
 # Rails.application.config.assets.precompile << %w(*.png *.jpg *.jpeg *.gif *.svg *.ico *.eot *.ttf *.wfoff)
 
 Rails.application.config.assets.precompile << Proc.new do |path|
-  if path =~ /\.(css|js|eot|svg|ttf|woff)\z/
+  if (File.extname(path) =~ /\.(css|js|eot|svg|ttf|woff|woff2|otf)$/)
     full_path = Pathname.new(Rails.application.assets.resolve(path)).to_s
     app_assets_path = Pathname.new(Rails.root.join('app', 'assets')).to_s
     full_path.start_with? app_assets_path
