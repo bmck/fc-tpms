274c2a3d068f3bfeaab59f06379c8877f2514cbd
Bill McKinnon <bill.mckinnon@bmck.org>
2015-09-20 21:51:52 +0000

    bug fix in handling font files

--- website/config/initializers/assets.rb
+++ website/config/initializers/assets.rb
@@ -9,21 +9,5 @@ Rails.application.config.assets.version = '1.00001'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-# Rails.application.config.assets.precompile << %w(*.png *.jpg *.jpeg *.gif *.svg *.ico *.eot *.ttf *.wfoff)
-
-Rails.application.config.assets.precompile << Proc.new do |path|
-  if (File.extname(path) =~ /\.(css|js|eot|svg|ttf|woff|woff2|otf)$/)
-    full_path = Pathname.new(Rails.application.assets.resolve(path)).to_s
-    app_assets_path = Pathname.new(Rails.root.join('app', 'assets')).to_s
-    # full_path.start_with? app_assets_path
-    if full_path.start_with? app_assets_path
-      puts "including asset: " + full_path
-      true
-    else
-      puts "excluding asset: " + full_path
-      false
-    end
-  else
-    false
-  end
-end
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.precompile << %w(*.png *.jpg *.jpeg *.gif *.svg *.ico *.eot *.ttf *.woff *.woff2)
