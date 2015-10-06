1fca22a13d772526f77d97c105ad7b0f38e48b72
Bill McKinnon <bill.mckinnon@bmck.org>
2015-10-06 16:30:37 +0000

    updated conf file based on http://forums.fedoraforum.org/showthread.php?t=97374

--- other/analyze_tpms2.c
+++ other/analyze_tpms2.c
@@ -14,11 +14,21 @@
 
 #include "analyze_tpms_log.h"
 
-#define SYMBOLS_PER_MSG BITS_PER_FREESCALE_MSG
-#define CACHE_SIZE 65535
+#define SYMBOLS_PER_MSG         BITS_PER_FREESCALE_MSG
+#define CACHE_SIZE              65535
 
-#define HIGHBIT 1
-#define LOWBIT 0
+#define HIGHBIT                 1
+#define LOWBIT                  0
+
+#define MIN_PRELUDE_HF_SAMPLES (0.001 * SAMPLE_RATE)
+#define MAX_PRELUDE_HF_SAMPLES (0.002 * SAMPLE_RATE)
+#define MIN_PRELUDE_LF_SAMPLES (0.001 * SAMPLE_RATE)
+#define MAX_PRELUDE_LF_SAMPLES (0.002 * SAMPLE_RATE)
+
+#define MIN_SAMPLES_PER_SYMBOL (0.00015 * SAMPLE_RATE)
+#define MAX_SAMPLES_PER_SYMBOL (0.00025 * SAMPLE_RATE)
+
+#define DELTA_PHI_DEFN (fmod(fabs(2.0 * M_PI + phi - prev_phi), 2.0 * M_PI))
 
 #define MARK_HERE LOGI("%s: (%s:%d) --> here", src_name, __FILE__, __LINE__);
 
@@ -68,25 +78,18 @@ char *fleet_analysis(char *fn) {
 
 	char* returned_string = malloc(250 * sizeof(char));
 	if (analyzed_ok == 1) {
-
 		LOGI("File analyzed ok");
 
 		char *hexaddr = NULL; hexaddr = get_hex_address_str(hexaddr);
-
 		LOGI("Got address");
-		// const char *const_hex = hexaddr;
-		// LOGI(const_hex);
 
 		long tempc = get_temp_c();
-
 		LOGI("Got temp and ...");
 
 		double press_kpa = get_pressure_kpa();
-
 		LOGI("Got pressure");
 
 		sprintf(returned_string, "%s,%ld,%lf,", hexaddr, tempc, press_kpa);
-
 	}
 
 	else {
@@ -122,16 +125,19 @@ int analyze_file(char *src_filename) {
 	strcpy(src_name, src_filename);
 
 	if (pkt) {
-		// LOGI("%s: (%s:%d) FINAL basebit_vals = %s\n", src_name, __FILE__, __LINE__, basebit_vals);
 		LOGI("%s: (%s:%d) FINAL symbols = %s", src_name, __FILE__, __LINE__, symbols);
 		return 1;
 	}
+
+	LOGI("%s: (%s:%d) FINAL symbols not found", src_name, __FILE__, __LINE__);
 	return 0;
 }
 
+
+
 int *get_packet() { //char *src_filename) {
 
-	int pkt_size = SYMBOLS_PER_MSG;
+	const int pkt_size = SYMBOLS_PER_MSG;
 	float tolerance = 0.05;
 
 	while (1) {
@@ -177,8 +183,9 @@ int *get_packet() { //char *src_filename) {
 
 	LOGI("%s: (%s:%d) --> Failed to find packet", src_name, __FILE__, __LINE__);
 	return NULL;
-
 }
+
+
 
 int *try_to_get_packet_symbols(unsigned int pkt_start, int num_syms, unsigned int est_samples_per_sym) {
 	write_to_cache = !read_from_cache;
@@ -188,13 +195,21 @@ int *try_to_get_packet_symbols(unsigned int pkt_start, int num_syms, unsigned in
 	}
 	valid_pkt = true;
 
+	if ((est_samples_per_sym > MAX_SAMPLES_PER_SYMBOL) || (est_samples_per_sym < MIN_SAMPLES_PER_SYMBOL)) {
+		LOGI("%s: (%s:%d) --> Invalid estimated samples/symbol = %d", src_name, __FILE__, __LINE__, est_samples_per_sym);
+		return NULL;
+	}
+
 	LOGI("%s: (%s:%d) --> Starting to try to get packet with samples/symbol = %d", src_name, __FILE__, __LINE__, est_samples_per_sym);
 	int *pkt_symbols = get_packet_symbols(pkt_start, num_syms, est_samples_per_sym);
 	if (pkt_symbols && valid_pkt) {
 		return pkt_symbols;
 	}
+
 	return NULL;
 }
+
+
 
 unsigned long find_prelude_start() {
 	float complex *x;
@@ -215,6 +230,8 @@ unsigned long find_prelude_start() {
 	return 0;
 }
 
+
+
 bool is_valid_prelude(unsigned long file_loc) {
 	double cum_phi = 0.0;
 	int count = 0;
@@ -234,22 +251,22 @@ bool is_valid_prelude(unsigned long file_loc) {
 			return false;
 		}
 		phi = cargf(*x);
-		delta_phi = fmod(fabs(2.0 * M_PI + phi - prev_phi), 2.0 * M_PI);
+		delta_phi = (DELTA_PHI_DEFN);
 
 		cum_phi += delta_phi;
 		count++;
 		high_freq = cum_phi / (double) count;
-
 		prev_phi = phi;
 		free(x);
 
-		if (count > 2000)  {
+		if (count > MAX_PRELUDE_HF_SAMPLES)  {
+			LOGI("%s: (%s:%d) ----> Excessively long HF phase of prelude found", src_name, __FILE__, __LINE__);
 			return false;
 		}
 
 		// LOGI("%s: (%s:%d) ----> total_samples_in = %lu, %u = prelude_hf_start + 0.001 * SAMPLE_RATE", src_name, __FILE__, __LINE__, total_samples_in, prelude_hf_start + (unsigned)(0.001 * SAMPLE_RATE));
 		// LOGI("%s: (%s:%d) ----> x = %f + i%f, delta_phi = %lf, cum_phi = %lf, count = %d, high_freq = %lf, fabs((delta_phi / high_freq) - 1.0) = %lf", src_name, __FILE__, __LINE__, crealf(*x), cimagf(*x), delta_phi, cum_phi, count, high_freq, fabs((delta_phi / high_freq) - 1.0));
-	} while ((total_samples_in < prelude_hf_start + 0.001 * SAMPLE_RATE) ||
+	} while ((total_samples_in < prelude_hf_start + MIN_PRELUDE_HF_SAMPLES) ||
 	         (fabs((delta_phi / high_freq) - 1.0) <= 0.05));
 
 	high_freq_known = true;
@@ -264,28 +281,28 @@ bool is_valid_prelude(unsigned long file_loc) {
 			return false;
 		}
 		phi = cargf(*x);
-		delta_phi = fmod(fabs(2.0 * M_PI + phi - prev_phi), 2.0 * M_PI);
-		// delta_phi = fmod(phi + M_PI * 0.5 - prev_phi, M_PI * 0.5);
+		delta_phi = (DELTA_PHI_DEFN);
 
 		cum_phi += delta_phi;
 		count++;
 		low_freq = cum_phi / (double) count;
-
 		prev_phi = phi;
 		free(x);
 
-		if (count > 2000)  {
+		if (count > MAX_PRELUDE_LF_SAMPLES)  {
+			LOGI("%s: (%s:%d) ----> Excessively long LF phase of prelude found", src_name, __FILE__, __LINE__);
 			return false;
 		}
 
 		// LOGI("%s: (%s:%d) ----> total_samples_in = %lu, %u = prelude_lf_start + 0.001 * SAMPLE_RATE", src_name, __FILE__, __LINE__, total_samples_in, prelude_lf_start + (unsigned)(0.001 * SAMPLE_RATE));
 		// LOGI("%s: (%s:%d) ----> x = %f + i%f, delta_phi = %lf, cum_phi = %lf, count = %d, low_freq = %lf, fabs((delta_phi / low_freq) - 1.0) = %lf", src_name, __FILE__, __LINE__, crealf(*x), cimagf(*x), delta_phi, cum_phi, count, low_freq, fabs((delta_phi / low_freq) - 1.0));
-	} while ((total_samples_in < prelude_lf_start + 0.001 * SAMPLE_RATE) ||
+	} while ((total_samples_in < prelude_lf_start + MIN_PRELUDE_LF_SAMPLES) ||
 	         (fabs((delta_phi / high_freq) - 1.0) > 0.10));
 
 	low_freq_known = true;
 	LOGI("%s: (%s:%d) ----> Found end of LF phase of prelude at sample %lu, low_freq = %lf", src_name, __FILE__, __LINE__, total_samples_in, low_freq);
 	if (high_freq <= low_freq) {
+		LOGI("%s: (%s:%d) ----> Invalid high_freq (%lf) and low_freq (%lf) combination found", src_name, __FILE__, __LINE__, high_freq, low_freq);
 		return false;
 	}
 
@@ -297,9 +314,7 @@ bool is_valid_prelude(unsigned long file_loc) {
 			return false;
 		}
 		phi = cargf(*x);
-		delta_phi = fmod(fabs(2.0 * M_PI + phi - prev_phi), 2.0 * M_PI);
-		// delta_phi = fmod(phi + M_PI * 0.5 - prev_phi, M_PI * 0.5);
-
+		delta_phi = (DELTA_PHI_DEFN);
 		prev_phi = phi;
 		free(x);
 
@@ -309,13 +324,17 @@ bool is_valid_prelude(unsigned long file_loc) {
 	sync_bits_end = total_samples_in;
 	samples_per_sym = sync_bits_end - sync_bits_start + 1;
 	LOGI("%s: (%s:%d) ----> Found end of 2nd HF phase of prelude at sample %lu", src_name, __FILE__, __LINE__, total_samples_in);
-	LOGI("%s: (%s:%d) ----> Estimating samples/symbol = %u", src_name, __FILE__, __LINE__, samples_per_sym);
-	if (samples_per_sym < 350) {
+
+	if (samples_per_sym < MIN_SAMPLES_PER_SYMBOL) {
+		LOGI("%s: (%s:%d) ----> Invalid samples/symbol found %u < %d", src_name, __FILE__, __LINE__, samples_per_sym, (int)MIN_SAMPLES_PER_SYMBOL);
 		return false;
 	}
 
+	LOGI("%s: (%s:%d) ----> Estimating samples/symbol = %u", src_name, __FILE__, __LINE__, samples_per_sym);
 	return true;
 }
+
+
 
 int *get_packet_symbols(unsigned int pkt_start, int num_syms, unsigned int est_samples_per_sym) {
 
@@ -326,7 +345,8 @@ int *get_packet_symbols(unsigned int pkt_start, int num_syms, unsigned int est_s
 	strcpy(symbols, "1");
 
 	LOGI("%s: (%s:%d) ------> Acquired symbol %d = %d, packet still valid", src_name, __FILE__, __LINE__, i, packet_syms[i]);
-
+	LOGI("%s: (%s:%d) ------> Bits collected = %s", src_name, __FILE__, __LINE__, bits);
+	LOGI("%s: (%s:%d) ------> Symbols collected = %s", src_name, __FILE__, __LINE__, symbols);
 
 
 	i = 1;
@@ -347,6 +367,7 @@ int *get_packet_symbols(unsigned int pkt_start, int num_syms, unsigned int est_s
 
 	for (i = 2; i < num_syms; i++) {
 		LOGI("%s: (%s:%d) ------> Starting to acquire symbol %d at sample %u", src_name, __FILE__, __LINE__, i, actual_bit_end + 1);
+
 		// actual_bit_end is set during the prev run of get_bit
 		packet_syms[i] = get_symbol(actual_bit_end + 1, est_samples_per_sym);
 		if (!valid_pkt) {
@@ -375,6 +396,8 @@ int *get_packet_symbols(unsigned int pkt_start, int num_syms, unsigned int est_s
 	return packet_syms;
 }
 
+
+
 bool validate_packet(int *packet_syms, int num_syms) {
 	LOGI("%s: (%s:%d) --------> Validating packet containing %d symbols", src_name, __FILE__, __LINE__, num_syms);
 
@@ -400,6 +423,8 @@ bool validate_packet(int *packet_syms, int num_syms) {
 
 	return true;
 }
+
+
 
 int get_second_symbol(unsigned int sym_start, unsigned int est_samples_per_sym) {
 	preceding_bit = 1;
@@ -477,6 +502,8 @@ int get_symbol(unsigned int sym_start, unsigned int est_samples_per_sym) {
 	return symbol;
 }
 
+
+
 int get_bit(unsigned long bit_start, unsigned long bit_end) {
 
 	unsigned long start_sample = bit_start;
@@ -485,9 +512,9 @@ int get_bit(unsigned long bit_start, unsigned long bit_end) {
 	LOGI("%s: (%s:%d) ----------> Starting to acquire bit between samples %lu and apx %lu (possibly as far as sample %lu)", src_name, __FILE__, __LINE__, bit_start, bit_end, (long unsigned) (bit_end + num_samples * 0.1));
 	complex float *samples_buffer = get_some_samples(num_samples * 1.1, bit_start);
 
-	int transition_point = scan_samples_for_transition(samples_buffer, num_samples * 1.1);
-	actual_bit_end = (transition_point == 0) ? bit_end : (transition_point - 1 + bit_start);
-	if (transition_point != 0) {
+	int transition_point = scan_samples_for_eob_transition(samples_buffer, num_samples * 1.1);
+	actual_bit_end = (transition_point == -1) ? bit_end : (transition_point - 1 + bit_start);
+	if (transition_point >= 0) {
 		LOGI("%s: (%s:%d) ----------> Transition found at sample %u", src_name, __FILE__, __LINE__, actual_bit_end);
 	}
 	if (write_to_cache) {
@@ -508,14 +535,16 @@ int get_bit(unsigned long bit_start, unsigned long bit_end) {
 	}
 	bit_confidence = fabs((double) b / (double) num_samples);
 
-	LOGI("%s: (%s:%d) ----------> num_samples = %d, b = %d", src_name, __FILE__, __LINE__, num_samples, b);
+	LOGI("%s: (%s:%d) ----------> num_samples = %d, b = %d, confidence = %f", src_name, __FILE__, __LINE__, num_samples, b, bit_confidence);
 
 	b = (b > 0) ? HIGHBIT : LOWBIT;
 	free(samples_buffer);
 	free(samples);
-	LOGI("%s: (%s:%d) ----------> bit returned (b) = %d", src_name, __FILE__, __LINE__, b);
+	LOGI("%s: (%s:%d) ----------> bit returned = %d", src_name, __FILE__, __LINE__, b);
 	return b;
 }
+
+
 
 int *convert_samples_to_binary(complex float *samples, int num_samples) {
 	int i, b = 0;
@@ -527,13 +556,11 @@ int *convert_samples_to_binary(complex float *samples, int num_samples) {
 	// LOGI("%s: (%s:%d) ------------> Starting to convert collected samples to binary", src_name, __FILE__, __LINE__);
 
 	for (i = 0; i < num_samples; i++) {
-		// MARK_HERE;
 		x = samples[i];
-		// MARK_HERE;
+
 		phi = cargf(x);
 		if (i > 0) {
-			delta_phi = fmod(fabs(2.0 * M_PI + phi - prev_phi), 2.0 * M_PI);
-			// delta_phi = fmod(phi + M_PI * 0.5 - prev_phi, M_PI * 0.5);
+			delta_phi = (DELTA_PHI_DEFN);
 			// LOGI("%s: (%s:%d) ------------> i = %d, sample no = %lu, delta_phi = %lf, high_freq = %lf, low_freq = %lf", src_name, __FILE__, __LINE__, i, cache_loc_in_file+i, delta_phi, high_freq, low_freq);
 
 			// if (fabs((delta_phi / high_freq) - 1.0) <= 0.05) {
@@ -568,7 +595,9 @@ int *convert_samples_to_binary(complex float *samples, int num_samples) {
 
 }
 
-int scan_samples_for_transition(float complex *samples, int num_samples) {
+
+
+int scan_samples_for_eob_transition(float complex *samples, int num_samples) {
 
 	LOGI("%s: (%s:%d) ----------> Starting to scan samples for end-of-bit transition", src_name, __FILE__, __LINE__);
 
@@ -585,7 +614,7 @@ int scan_samples_for_transition(float complex *samples, int num_samples) {
 	double later_freq, phi, later_delta_phi, prev_phi = cargf(samples[baseline_limit-1]);
 	for ( ; i < num_samples; i++) {
 		phi = cargf(samples[i]);
-		later_delta_phi = fmod(fabs(2.0 * M_PI + phi - prev_phi), 2.0 * M_PI);
+		later_delta_phi = (DELTA_PHI_DEFN);
 		prev_phi = phi;
 
 		later_freq = fabs((later_delta_phi/non_orig_freq) - 1.0);
@@ -596,8 +625,10 @@ int scan_samples_for_transition(float complex *samples, int num_samples) {
 		}
 	}
 	LOGI("%s: (%s:%d) ----------> Transition not found; assuming original bit end estimate", src_name, __FILE__, __LINE__);
-	return 0;
+	return -1;
 }
+
+
 
 float complex *get_some_samples(int num_samples, unsigned long start_sample_location) {
 	float complex *x;
@@ -629,10 +660,14 @@ float complex *get_some_samples(int num_samples, unsigned long start_sample_loca
 	return returned_samples;
 }
 
+
+
 unsigned long get_current_read_loc() {
 	unsigned long x = (read_from_cache ? (cache_loc_in_file + cache_loc) : total_samples_in);
 	return x;
 }
+
+
 
 // static FILE *file2;
 
@@ -714,7 +749,7 @@ int get_success() {
 	return (valid_pkt ? 1 : 0);
 }
 
-unsigned long get_dec_address_val( /*char *symbols, long num_bits*/ ) {
+unsigned long get_dec_address_val() {
 	INIT_LOGGING
 	unsigned long addr; addr = 0;
 	int j; for (j = 2; j <= 29; j++) { addr = addr * 2 + ((symbols[j] == '1') ? 1 : 0); }
@@ -722,15 +757,15 @@ unsigned long get_dec_address_val( /*char *symbols, long num_bits*/ ) {
 	return addr;
 }
 
-char *get_hex_address_str( /*char *symbols, long num_bits,*/ char *returned_url) {
+char *get_hex_address_str(char *returned_url) {
 	INIT_LOGGING
 	char *addr_str = malloc(sizeof(char) * 10);
-	sprintf(addr_str, "%lX", get_dec_address_val( /*symbols, num_bits */));
+	sprintf(addr_str, "%lX", get_dec_address_val());
 	LOGI("%s: (%s:%d) hex_addr = %s", src_name, __FILE__, __LINE__, addr_str);
 	return (returned_url = addr_str);
 }
 
-long get_temp_c( /*char *symbols, long num_bits*/ ) {
+long get_temp_c() {
 	INIT_LOGGING
 	long tempC; tempC = 0L;
 	int j; for (j = 52; j <= 59; j++) { tempC = tempC * 2 + ((symbols[j] == '1') ? 1 : 0); }
@@ -739,14 +774,14 @@ long get_temp_c( /*char *symbols, long num_bits*/ ) {
 	return tempC;
 }
 
-long get_temp_f( /*char *symbols, long num_bits*/ ) {
-	long tempC; tempC = get_temp_c( /*symbols, num_bits */);
+long get_temp_f() {
+	long tempC; tempC = get_temp_c();
 	long tempF = ((double)(tempC * 1.8)) + 32;
 	LOGI("%s: (%s:%d) tempF = %ld", src_name, __FILE__, __LINE__, tempF);
 	return tempF;
 }
 
-double get_pressure_kpa( /*char *symbols, long num_bits*/ ) {
+double get_pressure_kpa() {
 	INIT_LOGGING
 	double press; press = 0.0;
 	int j; for (j = 36; j <= 43; j++) { press = press * 2.0 + (symbols[j] == '1' ? 1.0 : 0.0); };
@@ -755,8 +790,8 @@ double get_pressure_kpa( /*char *symbols, long num_bits*/ ) {
 	return press;
 }
 
-double get_pressure_psi( /*char *symbols, long num_bits */ ) {
-	double psi = get_pressure_kpa( /*symbols, num_bits */) * 0.145037738;
+double get_pressure_psi() {
+	double psi = get_pressure_kpa() * 0.145037738;
 	LOGI("%s: (%s:%d) psi = %lf", src_name, __FILE__, __LINE__, psi);
 	return psi;
 }
--- other/analyze_tpms2.h
+++ other/analyze_tpms2.h
@@ -18,13 +18,11 @@
 
 #define UNK 0
 
-int get_success();
-// char *get_app_dir();
 
 char *fleet_analysis(char *fn);
 
 int analyze_file(char *src_filename);
-int *get_packet(); //char *src_filename);
+int *get_packet();
 bool validate_packet(int *packet_syms, int num_syms);
 bool is_valid_prelude(unsigned long file_loc);
 unsigned long find_prelude_start();
@@ -33,19 +31,20 @@ int *get_packet_symbols(unsigned int pkt_start, int num_bits, unsigned int sampl
 int get_second_symbol(unsigned int sym_start, unsigned int samples_per_sym);
 int get_symbol(unsigned int sym_start, unsigned int samples_per_sym);
 int get_bit(unsigned long bit_start, unsigned long bit_end);
-int scan_samples_for_transition(float complex *samples, int num_samples);
+int scan_samples_for_eob_transition(float complex *samples, int num_samples);
 int *convert_samples_to_binary(complex float *samples, int num_samples);
 
 unsigned long get_current_read_loc();
 float complex *get_some_samples(int num_samples, unsigned long start_sample_location);
 float complex *get_next_sample();
 
-unsigned long get_dec_address_val( /*char *bit_vals, long num_bits*/ );
-char *get_hex_address_str( /*char *bit_vals, long num_bits,*/ char *returned_url);
-long get_temp_c( /*char *bit_vals, long num_bits*/ );
-long get_temp_f( /*char *bit_vals, long num_bits*/ );
-double get_pressure_kpa( /*char *bit_vals, long num_bits*/ );
-double get_pressure_psi( /*char *bit_vals, long num_bits */ );
+int get_success();
+unsigned long get_dec_address_val();
+char *get_hex_address_str(char *returned_url);
+long get_temp_c();
+long get_temp_f();
+double get_pressure_kpa();
+double get_pressure_psi();
 // char *get_url( /*long addr, long press, long temp,*/ char *returned_url);
 
 #endif
